5b1c4b81076ed20ef1ce8aab57062ca19bb52800
olhafilipenko160695 <olyaao95@gmail.com>
2023-10-27 09:34:47 +0200

    1

---
+++ js/task-3.js
@@ -0,0 +1,21 @@
+//* Задача 3. Фільтрація масиву чисел *//
+
+function filterArray(numbers, value) {
+    let newArray = [];
+    for (number of numbers) {
+        if (number > value) {
+         newArray.push(number)
+        }
+        else {
+         newArray;
+        }
+    }
+    return newArray
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
